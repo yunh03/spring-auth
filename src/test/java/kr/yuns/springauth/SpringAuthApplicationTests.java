d211e987913a1c03ff376fd9883e0d0ee094f7e5
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-12 02:21:59 +0900

    Initialize Project

---
+++ src/test/java/kr/yuns/springauth/SpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yuns.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
